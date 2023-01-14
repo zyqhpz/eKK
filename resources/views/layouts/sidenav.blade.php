a40af04e9d6ae842aec00a21a38f8c0a3778bfee
Haziq Hapiz <zyqq.dev@gmail.com>
2023-01-15 01:55:13 +0800

    remove other navigation

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -40,47 +40,17 @@
           </span>
         </a>
       </li>
-      <li class="nav-item {{ Request::segment(1) == 'dashboard' ? 'active' : '' }}">
+      {{-- <li class="nav-item {{ Request::segment(1) == 'dashboard' ? 'active' : '' }}">
         <a href="/dashboard" class="nav-link">
-          <span class="sidebar-icon"> <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-            </svg></span></span>
+          <span class="sidebar-icon"><i class="far fa-chart-pie"></i></span>
           <span class="sidebar-text">Dashboard</span>
         </a>
-      </li>
-      <li class="nav-item">
-        <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
-          data-bs-target="#submenu-pages">
-          <span>
-            <span class="sidebar-icon"><svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                  clip-rule="evenodd"></path>
-                <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-              </svg></span>
-            <span class="sidebar-text">Kelab</span>
-          </span>
-          <span class="link-arrow"><svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"></path>
-            </svg></span>
-        </span>
-        <div class="multi-level collapse {{ Request::segment(1) == 'profile' || Request::segment(1) == 'generator' || Request::segment(1) == 'paperwork' }}"
-        role="list" id="submenu-pages" aria-expanded="false">
-          <ul class="flex-column nav">
-            <li class="nav-item {{ Request::segment(1) == 'paperwork' ? 'active' : '' }}"">
-              <a class="nav-link d-flex align-items-center justify-content-between" href="{{ route('paperwork-club') }}">
-                <span class="sidebar-text">Kertas Kerja</span>
-                <span class="badge badge-sm bg-danger badge-pill notification-count">4</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+      </li> --}}
+      <li class="nav-item {{ Request::segment(1) == 'paperwork' ? 'active' : '' }}">
+        <a href="{{ route('paperwork-club') }}" class="nav-link">
+          <span class="sidebar-icon"><i class="far fa-newspaper"></i></span>
+          <span class="sidebar-text">Kertas Kerja</span>
+        </a>
       </li>
     </ul>
   </div>
